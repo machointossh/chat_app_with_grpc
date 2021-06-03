872183ed4c972358326df9c9f563afdce9ffb805
BotamochiRice <botamochi.rice@gmail.com>
2021-06-03 23:28:04 +0900

    *.proto moved to proto/ dir

--- protoc/protoc.sh
+++ protoc/protoc.sh
@@ -11,8 +11,8 @@ mkdir -p $SERVER_OUTPUT_DIR
 mkdir -p $CLIENT_OUTPUT_DIR
 
 protoc --version
-protoc $WORKDIR/messenger.proto \
-  --proto_path=$WORKDIR \
+protoc $WORKDIR/proto/messenger.proto \
+  --proto_path=$WORKDIR/proto \
   --go_out=plugins="grpc:${SERVER_OUTPUT_DIR}" \
   --js_out=import_style=commonjs:${CLIENT_OUTPUT_DIR} \
   --grpc-web_out=import_style=typescript,mode=grpcwebtext:${CLIENT_OUTPUT_DIR}
