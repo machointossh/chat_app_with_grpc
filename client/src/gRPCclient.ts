5041fdae09a93061857eebc992fda3c1307383cd
BotamochiRice <botamochi.rice@gmail.com>
2021-06-08 21:52:49 +0900

    ip for docker-compose

--- client/src/gRPCclient.ts
+++ client/src/gRPCclient.ts
@@ -6,4 +6,5 @@ export type GRPCClients = {
 
 export const gRPCClients = {
   messengerClient: new MessengerClient(`http://localhost:10000`),
+  // You need 'http://' and ':<PORT>' even if it is 80.
 };
