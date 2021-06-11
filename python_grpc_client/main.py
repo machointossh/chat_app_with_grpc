e83cfab92887c41563486917433717b785aa6bb4
BotamochiRice <botamochi.rice@gmail.com>
2021-06-11 11:56:24 +0900

    add timestamp to message response

--- python_grpc_client/main.py
+++ python_grpc_client/main.py
@@ -1,17 +1,24 @@
 import grpc
-import messenger_pb2
-import messenger_pb2_grpc
+from google.protobuf.timestamp_pb2 import Timestamp
+from proto import messenger_pb2
+from proto import messenger_pb2_grpc
 from time import sleep
+
 
 if __name__ == "__main__":
     for retry in range(5):
         try:
             with grpc.insecure_channel('localhost:10000') as channel:
+                # Post "Hello" message first
                 stub = messenger_pb2_grpc.MessengerStub(channel)
                 stub.PostMessage(messenger_pb2.MessageRequest(message="Hello"))
 
+                # Then keep receiving messages
                 for res in stub.FetchMessage(messenger_pb2.MessageResponse()):
-                    print(res.message)
+                    msg: str = res.message
+                    ts: Timestamp = res.timestamp
+                    formatted_ts = ts.ToDatetime().strftime("%Y/%m/%d %H:%M:%S")
+                    print(f"{res.message} ({formatted_ts})")
         except (KeyboardInterrupt, SystemExit):
             print('Shutting down gracefully...')
             break
--- python_grpc_client/proto/messenger_pb2_grpc.py
+++ python_grpc_client/proto/messenger_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
-import messenger_pb2 as messenger__pb2
+from proto import messenger_pb2 as proto_dot_messenger__pb2
 
 
 class MessengerStub(object):
@@ -18,12 +18,12 @@ class MessengerStub(object):
         self.FetchMessage = channel.unary_stream(
                 '/messsenger.Messenger/FetchMessage',
                 request_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
-                response_deserializer=messenger__pb2.MessageResponse.FromString,
+                response_deserializer=proto_dot_messenger__pb2.MessageResponse.FromString,
                 )
         self.PostMessage = channel.unary_unary(
                 '/messsenger.Messenger/PostMessage',
-                request_serializer=messenger__pb2.MessageRequest.SerializeToString,
-                response_deserializer=messenger__pb2.MessageResponse.FromString,
+                request_serializer=proto_dot_messenger__pb2.MessageRequest.SerializeToString,
+                response_deserializer=proto_dot_messenger__pb2.MessageResponse.FromString,
                 )
 
 
@@ -48,12 +48,12 @@ def add_MessengerServicer_to_server(servicer, server):
             'FetchMessage': grpc.unary_stream_rpc_method_handler(
                     servicer.FetchMessage,
                     request_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
-                    response_serializer=messenger__pb2.MessageResponse.SerializeToString,
+                    response_serializer=proto_dot_messenger__pb2.MessageResponse.SerializeToString,
             ),
             'PostMessage': grpc.unary_unary_rpc_method_handler(
                     servicer.PostMessage,
-                    request_deserializer=messenger__pb2.MessageRequest.FromString,
-                    response_serializer=messenger__pb2.MessageResponse.SerializeToString,
+                    request_deserializer=proto_dot_messenger__pb2.MessageRequest.FromString,
+                    response_serializer=proto_dot_messenger__pb2.MessageResponse.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -78,7 +78,7 @@ class Messenger(object):
             metadata=None):
         return grpc.experimental.unary_stream(request, target, '/messsenger.Messenger/FetchMessage',
             google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
-            messenger__pb2.MessageResponse.FromString,
+            proto_dot_messenger__pb2.MessageResponse.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
 
@@ -94,7 +94,7 @@ class Messenger(object):
             timeout=None,
             metadata=None):
         return grpc.experimental.unary_unary(request, target, '/messsenger.Messenger/PostMessage',
-            messenger__pb2.MessageRequest.SerializeToString,
-            messenger__pb2.MessageResponse.FromString,
+            proto_dot_messenger__pb2.MessageRequest.SerializeToString,
+            proto_dot_messenger__pb2.MessageResponse.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
